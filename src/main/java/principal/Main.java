9f494c974e54fc378287117012a4b25e15ac2044
Nicolas Aguilar <n.aguilar02@ufrontera.cl>
2023-12-03 17:51:12 -0300

    principal

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -7,23 +7,20 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(() -> {
-            String[][] datos = {{"Dato1", "Dato2"}, {"Dato3", "Dato4"}};
-            String[] nombreColumnas = {"Columna1", "Columna2"};
-
-        Ventana ventana1= new Ventana("Menu Intranet",400,500);
-        //VentanaTabla ventanaTabla1 = new VentanaTabla(datos,nombreColumnas);
-
-
-
-
-
-
-
-
-
-
-
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MenuPrincipal();
+            }
         });
+
     }
+
+
+
+
+
+
+
+
 }
